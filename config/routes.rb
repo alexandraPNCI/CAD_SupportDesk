2c7954e6dc9c17d572716d06da7e436a4a9e2ee4
alexandraPNCI <x20245823@student.ncirl.ie>
2025-12-09 17:02:48 +0000

    Add Bootstrap JavaScript

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :tickets do
-    resources :comments, only: [:create, :destroy]
-  end
-
-  get "up" => "rails/health#show", as: :rails_health_check
-  root "tickets#index"
+authenticated :user do
+  root to: "tickets#index", as: :authenticated_root
 end
 
+unauthenticated do
+  root to: "devise/sessions#new", as: :unauthenticated_root
+end
