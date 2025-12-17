3e1acc2af381e3d3e85658a966d14d351c1865f3
alexandraPNCI <x20245823@student.ncirl.ie>
2025-12-17 21:46:15 +0000

    Fix comments controller to assign user and save content

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,22 @@
+# x20245823 Alexandra
+# This handles all of the comment actions
+# Actions: create and destroy comments on tickets
+# Access is restricted to authenticated users only
+
 class CommentsController < ApplicationController
   before_action :authenticate_user!
 
+  # Create a comment for a specific ticket
   def create
     @ticket = Ticket.find(params[:ticket_id])
     @comment = @ticket.comments.build(comment_params)
     @comment.user = current_user
 
-    authorize @comment
-
     if @comment.save
-      redirect_to @ticket, notice: "Comment added."
+      redirect_to @ticket, notice: "Comment added"
     else
-      redirect_to @ticket, alert: "Comment could not be saved."
+      redirect_to @ticket, alert: @comment.errors.full_messages.to_sentence
     end
-  end
-
-  def destroy
-    @ticket = Ticket.find(params[:ticket_id])
-    @comment = @ticket.comments.find(params[:id])
-
-    authorize @comment
-
-    @comment.destroy
-    redirect_to @ticket, notice: "Comment deleted."
   end
 
   private
